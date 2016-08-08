24defdb1599fdecc236e2aa79ad738e526e125ba
krislippe <ubuntu@ip-172-31-67-247.ec2.internal>
2016-08-08 12:36:24 +0000

    remote static S3 image block

--- myproject.py
+++ myproject.py
@@ -1,3 +1,4 @@
+import boto3
 from flask import Flask, render_template
 from datetime import datetime
 
@@ -8,6 +9,27 @@ application = Flask(__name__)
 
 def hello():
     now = datetime.now()
+    s3 = boto3.resource('s3')
+
+    images = []
+    mybucket = s3.Bucket('lippe-mbr')
+
+    # grab images from s3
+    for object in mybucket.objects.all():
+        images.append('<img src="https://d1csbuudyxotp4.cloudfront.net/' + object.key + '" width="300" heigh="200">')
+
+    imagehtml = []
+    imagehtml.append('<table border=1>')
+
+    # build html table in rows of 3    
+    for i,pic in enumerate(images):
+        if i % 3 == 0:
+            imagehtml.append('<tr>')
+        imagehtml.append('<td>' + pic + '</td>')
+        if i %3 == 2 or i == (len(images) - 1):
+            imagehtml.append('</tr>')
+
+    imagehtml.append('</table>')
 
     user = {'nickname': 'Kris'}
     posts = [  # fake array of posts
@@ -23,7 +45,8 @@ def hello():
     return render_template("index.html",
                            title='Home',
                            user=user,
-                           posts=posts)
+                           posts=posts,
+                           images=imagehtml)
 
 if __name__ == "__main__":
     application.run(host='0.0.0.0')
