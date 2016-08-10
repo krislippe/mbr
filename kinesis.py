b466757c0567fec841437effe4796972389e83c7
krislippe <lippek@amazon.com>
2016-08-10 10:19:10 -0400

    initial version

---
+++ kinesis.py
@@ -0,0 +1,11 @@
+import boto3
+iam = boto3.client('iam')
+firehose = boto3.client('firehose')
+
+with open('/Users/lippek/Documents/weblogs/aws-big-data', 'r') as f:
+    for line in f:
+        firehose.put_record(
+                DeliveryStreamName='demo-firehose-stream',
+                Record={'Data': line}
+                )
+        print 'Record added'
